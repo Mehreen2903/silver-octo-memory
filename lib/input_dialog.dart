e38130a1e3d9f4f0b3c77cfd9cc5c1f23f332455
Mehreen <mehreen2903@gmail.com>
2023-11-11 14:07:29 +0530

    Updated repeating task alert input dialog

--- lib/input_dialog.dart
+++ lib/input_dialog.dart
@@ -290,77 +290,80 @@ void showNewTaskDialog(BuildContext context) {
 }
 
 //REPEATING TASK
+// Enums for the repeating task types and intervals
 enum RepeatingTaskType { goal, nonGoal }
 enum IntervalType { days, weeks, months }
+enum WeekDay { mon, tue, wed, thu, fri, sat, sun }
 
+// Extension method to capitalize strings.
+extension StringExtension on String {
+  String capitalize() {
+    return "${this[0].toUpperCase()}${substring(1)}";
+  }
+}
+
+// Function to show the dialog
 void showNewRepeatingTaskDialog(BuildContext context) {
   TextEditingController taskTitleController = TextEditingController();
   TextEditingController descriptionController = TextEditingController();
   TextEditingController frequencyController = TextEditingController();
+  TextEditingController dayOfMonthController = TextEditingController();
   RepeatingTaskType selectedTaskType = RepeatingTaskType.goal;
-  IntervalType selectedIntervalType = IntervalType.days;
-  DateTime? selectedDate;
-  TimeOfDay? selectedTime;
+  IntervalType? selectedIntervalType;
+  WeekDay? selectedWeekDay;
+  DateTime? startDate;
+  DateTime? endDate;
 
   showDialog(
     context: context,
     builder: (BuildContext context) {
       return StatefulBuilder(
         builder: (context, setState) {
-          Future<void> _selectDate(BuildContext context) async {
+          Future<void> _selectStartDate(BuildContext context) async {
             final DateTime? picked = await showDatePicker(
               context: context,
-              initialDate: selectedDate ?? DateTime.now(),
+              initialDate: startDate ?? DateTime.now(),
               firstDate: DateTime(2000),
               lastDate: DateTime(2101),
             );
-            if (picked != null && picked != selectedDate) {
+            if (picked != null && picked != startDate) {
               setState(() {
-                selectedDate = picked;
+                startDate = picked;
               });
             }
           }
 
-          Future<void> _selectTime(BuildContext context) async {
-            final TimeOfDay? picked = await showTimePicker(
+          Future<void> _selectEndDate(BuildContext context) async {
+            final DateTime? picked = await showDatePicker(
               context: context,
-              initialTime: selectedTime ?? TimeOfDay.now(),
+              initialDate: endDate ?? (startDate ?? DateTime.now()),
+              firstDate: startDate ?? DateTime.now(),
+              lastDate: DateTime(2101),
             );
-            if (picked != null && picked != selectedTime) {
+            if (picked != null && picked != endDate) {
               setState(() {
-                selectedTime = picked;
+                endDate = picked;
               });
             }
           }
 
-          // List<Widget> buildIntervalTypeChips() {
-          //   return IntervalType.values.map((interval) {
-          //     return ChoiceChip(
-          //       label: Text(describeEnum(interval).capitalize()),
-          //       selected: selectedIntervalType == interval,
-          //       selectedColor: Theme.of(context).colorScheme.primary,
-          //       onSelected: (bool isSelected) {
-          //         setState(() {
-          //           if (isSelected) {
-          //             selectedIntervalType = interval;
-          //           }
-          //         });
-          //       },
-          //
-          //       labelStyle: TextStyle(
-          //         color: selectedIntervalType == interval
-          //             ? Colors.white
-          //             : Theme.of(context).colorScheme.onSurface,
-          //       ),
-          //       backgroundColor: Theme.of(context).brightness == Brightness.dark
-          //           ? Colors.grey[700]
-          //           : Colors.grey[300],
-          //       checkmarkColor: Theme.of(context).brightness == Brightness.dark
-          //           ? Colors.white
-          //           : Colors.black,
-          //     );
-          //   }).toList();
-          // }
+          List<DropdownMenuItem<IntervalType>> buildIntervalDropdownItems() {
+            return IntervalType.values.map((interval) {
+              return DropdownMenuItem(
+                value: interval,
+                child: Text(describeEnum(interval).capitalize()),
+              );
+            }).toList();
+          }
+
+          List<DropdownMenuItem<WeekDay>> buildWeekDayDropdownItems() {
+            return WeekDay.values.map((weekDay) {
+              return DropdownMenuItem(
+                value: weekDay,
+                child: Text(describeEnum(weekDay).capitalize()),
+              );
+            }).toList();
+          }
 
           return AlertDialog(
             title: Text('New Repeating Task'),
@@ -391,7 +394,7 @@ void showNewRepeatingTaskDialog(BuildContext context) {
                         label: Text('Goal'),
                         selected: selectedTaskType == RepeatingTaskType.goal,
                         selectedColor: Theme.of(context).colorScheme.primary,
-                        onSelected: (bool selected) {
+                        onSelected: (bool isSelected) {
                           setState(() {
                             selectedTaskType = RepeatingTaskType.goal;
                           });
@@ -399,21 +402,19 @@ void showNewRepeatingTaskDialog(BuildContext context) {
                         labelStyle: TextStyle(
                           color: selectedTaskType == RepeatingTaskType.goal
                               ? Colors.white
-                              : Theme.of(context).colorScheme.onSurface,
+                              : Theme.of(context).textTheme.bodyLarge?.color,
                         ),
                         backgroundColor: Theme.of(context).brightness == Brightness.dark
                             ? Colors.grey[700]
                             : Colors.grey[300],
-                        checkmarkColor: Theme.of(context).brightness == Brightness.dark
-                            ? Colors.white
-                            : Colors.black,
+                        checkmarkColor: Colors.white,  // Checkmark color is always white for visibility
                       ),
                       SizedBox(width: 8.0),
                       ChoiceChip(
                         label: Text('Non-Goal'),
                         selected: selectedTaskType == RepeatingTaskType.nonGoal,
                         selectedColor: Theme.of(context).colorScheme.primary,
-                        onSelected: (bool selected) {
+                        onSelected: (bool isSelected) {
                           setState(() {
                             selectedTaskType = RepeatingTaskType.nonGoal;
                           });
@@ -421,71 +422,78 @@ void showNewRepeatingTaskDialog(BuildContext context) {
                         labelStyle: TextStyle(
                           color: selectedTaskType == RepeatingTaskType.nonGoal
                               ? Colors.white
-                              : Theme.of(context).colorScheme.onSurface,
+                              : Theme.of(context).textTheme.bodyLarge?.color,
                         ),
                         backgroundColor: Theme.of(context).brightness == Brightness.dark
                             ? Colors.grey[700]
                             : Colors.grey[300],
-                        checkmarkColor: Theme.of(context).brightness == Brightness.dark
-                            ? Colors.white
-                            : Colors.black,
+                        checkmarkColor: Colors.white,  // Checkmark color is always white for visibility
                       ),
                     ],
                   ),
                   SizedBox(height: 8.0),
                   ListTile(
-                    title: Text('Date'),
-                    subtitle: Text(selectedDate == null ? 'Pick a date' : DateFormat('yyyy-MM-dd').format(selectedDate!)),
+                    title: Text('Start Date'),
+                    subtitle: Text(startDate == null ? 'Pick a start date' : DateFormat('yyyy-MM-dd').format(startDate!)),
                     trailing: Icon(Icons.calendar_today),
-                    onTap: () => _selectDate(context),
+                    onTap: () => _selectStartDate(context),
                   ),
                   ListTile(
-                    title: Text('Time'),
-                    subtitle: Text(selectedTime == null ? 'Pick a time' : selectedTime!.format(context)),
-                    trailing: Icon(Icons.access_time),
-                    onTap: () => _selectTime(context),
+                    title: Text('End Date'),
+                    subtitle: Text(endDate == null ? 'Pick an end date' : DateFormat('yyyy-MM-dd').format(endDate!)),
+                    trailing: Icon(Icons.calendar_today),
+                    onTap: () => _selectEndDate(context),
                   ),
                   SizedBox(height: 8.0),
-                  TextField(
-                    controller: frequencyController,
+                  DropdownButtonFormField<IntervalType>(
                     decoration: InputDecoration(
-                      labelText: 'Frequency',
+                      labelText: 'Interval',
                       border: OutlineInputBorder(),
-                      hintText: 'e.g., Every 1 week(s)',
                     ),
-                    keyboardType: TextInputType.number,
+                    value: selectedIntervalType,
+                    onChanged: (IntervalType? newValue) {
+                      setState(() {
+                        selectedIntervalType = newValue;
+                      });
+                    },
+                    items: buildIntervalDropdownItems(),
                   ),
-                  SizedBox(height: 8.0),
-                  Wrap(
-                    spacing: 8.0,
-                    children: IntervalType.values.map((interval) {
-                      return ChoiceChip(
-                        label: Text(describeEnum(interval).capitalize()),
-                        selected: selectedIntervalType == interval,
-                        selectedColor: Theme.of(context).colorScheme.primary,
-                        onSelected: (bool selected) {
-                          setState(() {
-                            selectedIntervalType = interval;
-                          });
-                        },
-                        labelStyle: TextStyle(
-                          color: selectedIntervalType == interval
-                              ? Colors.white
-                              : Theme.of(context).brightness == Brightness.dark
-                              ? Colors.white
-                              : Colors.black,
+                  if (selectedIntervalType == IntervalType.weeks)
+                    DropdownButtonFormField<WeekDay>(
+                      decoration: InputDecoration(
+                        labelText: 'Day of the Week',
+                        border: OutlineInputBorder(),
+                      ),
+                      value: selectedWeekDay,
+                      onChanged: (WeekDay? newValue) {
+                        setState(() {
+                          selectedWeekDay = newValue;
+                        });
+                      },
+                      items: buildWeekDayDropdownItems(),
+                    ),
+                  if (selectedIntervalType == IntervalType.months)
+                    TextField(
+                      controller: dayOfMonthController,
+                      decoration: InputDecoration(
+                        labelText: 'Day(s) of the Month',
+                        border: OutlineInputBorder(),
+                        hintText: 'e.g., 15, or 1,15,30',
+                      ),
+                    ),
+                  if (selectedIntervalType == IntervalType.days)
+                    Padding(
+                      padding: const EdgeInsets.only(top: 8.0),
+                      child: TextField(
+                        controller: frequencyController,
+                        decoration: InputDecoration(
+                          labelText: 'Frequency',
+                          border: OutlineInputBorder(),
+                          hintText: 'e.g., Every 2 days',
                         ),
-                        backgroundColor: selectedIntervalType == interval
-                            ? Theme.of(context).colorScheme.primary.withOpacity(0.3)
-                            : Theme.of(context).brightness == Brightness.dark
-                            ? Colors.grey[700]
-                            : Colors.grey[300],
-                        checkmarkColor: Theme.of(context).brightness == Brightness.dark
-                            ? Colors.white
-                            : Colors.black,
-                      );
-                    }).toList(),
-                  ),
+                        keyboardType: TextInputType.number,
+                      ),
+                    ),
                 ],
               ),
             ),
@@ -496,10 +504,12 @@ void showNewRepeatingTaskDialog(BuildContext context) {
                   // Handle the repeating task submission
                   print('Task Title: ${taskTitleController.text}');
                   print('Description: ${descriptionController.text}');
-                  print('Date: ${selectedDate?.toIso8601String()}');
-                  print('Time: ${selectedTime?.format(context)}');
+                  print('Start Date: ${startDate?.toIso8601String()}');
+                  print('End Date: ${endDate?.toIso8601String()}');
                   print('Frequency: ${frequencyController.text}');
-                  print('Interval: ${describeEnum(selectedIntervalType)}');
+                  print('Interval: ${selectedIntervalType != null ? describeEnum(selectedIntervalType!) : 'None'}');
+                  print('Day of the Week: ${selectedWeekDay != null ? describeEnum(selectedWeekDay!) : 'None'}');
+                  print('Day(s) of the Month: ${dayOfMonthController.text}');
                   Navigator.pop(context); // Dismiss the dialog
                 },
               ),
@@ -511,9 +521,10 @@ void showNewRepeatingTaskDialog(BuildContext context) {
   );
 }
 
-// Extension method to capitalize strings.
-extension StringExtension on String {
-  String capitalize() {
-    return "${this[0].toUpperCase()}${substring(1)}";
-  }
-}
+
+
+
+
+
+
+
